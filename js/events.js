5d54b6fc681d5ce93a4c2c591a17679fb201a045
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 13:40:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,6 +9,13 @@ function frameIt(){
     $('img').addClass("tasty");
   })
 }
+function pressIt(){
+  $('input').on('keydown', (e)=>{
+    if (parseInt(e.which || e.detail) === 71){
+      alert('You have successfully found a G-spot!')
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
